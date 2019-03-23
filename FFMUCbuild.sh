5c3f24e862cc945a3704cc71e4a115855d93227d
Tobias <T0biii@users.noreply.github.com>
2019-03-23 15:04:12 +0100

    Update FFMUCbuild.sh

--- FFMUCbuild.sh
+++ FFMUCbuild.sh
@@ -2,14 +2,14 @@
 
 # Populate variables with default values
 
-GLUON_GIT_REF=master 
+GLUON_GIT_REF=master
 GLUON_GIT=https://github.com/freifunk-gluon/gluon.git
 GLUON_BUILD_DIR=gluon-build
 GLUON_RELEASE=t0biii.2019.0-exp~dock08
 OPENWRT_DIR=$(pwd)/openwrt
 OUTPUT_DIR=$(pwd)/output
 SECRET_KEY_FILE=$(pwd)/key/secret
-GLUON_BRANCH=experimental 
+GLUON_BRANCH=experimental
 GLUON_TARGETS=x86-generic x86-geode x86-64
 
 
