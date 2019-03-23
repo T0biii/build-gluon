9c558c348efeb1148a352c6da2044f3be625776b
Tobias <T0biii@users.noreply.github.com>
2019-03-23 14:54:31 +0100

    Update FFMUCbuild.sh

--- FFMUCbuild.sh
+++ FFMUCbuild.sh
@@ -5,11 +5,12 @@
 GLUON_GIT_REF=master 
 GLUON_GIT=https://github.com/freifunk-gluon/gluon.git
 GLUON_BUILD_DIR=gluon-build
-GLUON_RELEASE=t0biii.2019.0-exp~dock06
+GLUON_RELEASE=t0biii.2019.0-exp~dock08
 OPENWRT_DIR=$(pwd)/openwrt
 OUTPUT_DIR=$(pwd)/output
+SECRET_KEY_FILE=$(pwd)/key/secret
 GLUON_BRANCH=experimental 
 GLUON_TARGETS=x86-generic x86-geode x86-64
 
 
-docker run --rm -a STDOUT -a STDERR -v $OPENWRT_DIR:/gluon/openwrt -v $OUTPUT_DIR:/gluon/output -w /gluon build-gluon /bin/bash -c "make sign GLUON_RELEASE=$GLUON_RELEASE GLUON_BRANCH=$GLUON_BRANCH GLUON_GIT_URL=$GLUON_GIT GLUON_GIT_REF=$GLUON_GIT_REF GLUON_TARGETS=$GLUON_TARGETS"
+docker run --rm -a STDOUT -a STDERR -v $OPENWRT_DIR:/gluon/openwrt -v $OUTPUT_DIR:/gluon/output -v $KEY_DIR:/gluon/key  -w /gluon build-gluon /bin/bash -c "make sign GLUON_RELEASE=$GLUON_RELEASE GLUON_BRANCH=$GLUON_BRANCH GLUON_GIT_URL=$GLUON_GIT GLUON_GIT_REF=$GLUON_GIT_REF GLUON_TARGETS=$GLUON_TARGETS SECRET_KEY_FILE=$SECRET_KEY_FILE"
